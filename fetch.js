003183fd9e4aa9f4016669e4996809c74a405968
Victor Joh <victor.joh@snapraise.com>
2022-03-09 15:21:33 -0600

    Initial commit for fetch_api project

---
+++ fetch.js
@@ -0,0 +1,15 @@
+/// Get Method //
+const getApi = "https://api.quotable.io/random";
+
+const getQuote = async () => {
+  //Add your get quote logic here
+};
+
+/// Post Method //
+const postApi = "https://fetch-quotes.herokuapp.com/quote";
+
+const postQuote = async () => {
+  const inputValue = document.getElementById("text-input").value;
+
+  //Add your post quote logic here
+};
