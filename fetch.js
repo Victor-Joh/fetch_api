6c62d5acb0f47f477760585b846cdc7afc6473b5
Victor Joh <victor.joh@snapraise.com>
2022-03-16 18:31:32 -0500

    Update comments in fetch.js

--- fetch.js
+++ fetch.js
@@ -1,17 +1,16 @@
-/// Get Method //
+// Get Method
 const getApi = "https://api.quotable.io/random";
 
 const getQuote = async () => {
-  //Add your GET quote logic here
+  // Add your GET quote logic here
 };
 
-/// Post Method //
-///Example Post Body
+// Post Method
 //{ quote: "some random quote" }
 const postApi = "https://fetch-quotes.herokuapp.com/quote";
 
 const postQuote = async () => {
   const inputValue = document.getElementById("text-input").value;
 
-  //Add your POST quote logic here
+  // Add your POST quote logic here
 };
